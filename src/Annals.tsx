2936163274afb75abf5d69f8f3850ac7c9e566d4
Billy Dean <billy.goehring@gmail.com>
2022-12-12 16:37:32 -0800

    fiddling with types

--- src/Annals.tsx
+++ src/Annals.tsx
@@ -1,14 +1,8 @@
-import * as types from './types';
-import { useState } from 'react';
 
 function Annals() {
-    const startingYear: types.Year = {
-        current:1,
-    }
-    const [year, setYear] = useState(startingYear);
     return (
             <div>
-                The year is { year.current }.
+                The year is....
                 </div>
     )
 };
--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,44 @@
-interface Year {
-    current: number,
+interface Plague {
+    name: string,
+    effect: string
 }
 
+interface War {
+    name: string,
+    sd: number, //starting date
+    participants: string[]
+}
+interface Year {
+    current: number,
+    total: number,
+    global_stability: number,
+    region_stability: number,
+    local_stability: number,
+    plague_now: boolean,
+    plagues: Plague[],
+    climate: string,
+    crops: string,
+    crop_fortune: number,
+    war_now: boolean,
+    wars: War[]
+}
 
+interface Person {
+    name: string,
+    id: number,
+    old_year: number, //death date by old age, barring other cause(s)
+    family_id: number,
+    mother_id: number,
+    father_id: number,
+    fertility: number,
+    alive: boolean,
+    offspring?: number[],
+    birth_year: number,
+    death_year?: number 
+}
 
-export type { Year };
+type People = Person[];
+
+export type { Year, Person, People, War, Plague };
 
 
