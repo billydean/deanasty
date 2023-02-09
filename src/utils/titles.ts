602d989bc34f4bf97bf66de6be37d75a2f284ca4
Billy Dean <billy.goehring@gmail.com>
2023-02-08 17:18:21 -0800

    Next steps on todo list

--- src/utils/titles.ts
+++ src/utils/titles.ts
@@ -14,3 +14,19 @@ export function firstTitle(year: number): Title {
         seat: 'Examplia'
     }
 }
+
+/**TODO
+ * 
+ * 1. Add title to worldgen and first person
+ * 2. Death Handler: Person have title? Is there an heir? Add heir to title and title to heir. Add new heir to title. Inheritance news.
+ * 3. ^^ First-- succession handler. Will determine what to do at death, etc.
+ * 4. Birth check: Parent have title? Meet conditions for heir? Heir news.
+ * 
+ * 
+ * will involve
+ *      titles
+ *      checks
+ *      people
+ *      masterFunctions
+ *      reducer
+ */
